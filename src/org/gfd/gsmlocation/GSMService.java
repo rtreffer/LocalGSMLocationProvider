e804e71299fde800da1161f6eb3d3ea9335d0d10
Rene Treffer <treffer@measite.de>
2014-03-12 09:34:35 +0100

    Move startup code to startup method

--- src/org/gfd/gsmlocation/GSMService.java
+++ src/org/gfd/gsmlocation/GSMService.java
@@ -7,10 +7,8 @@ import org.gfd.gsmlocation.model.CellInfo;
 import org.microg.nlp.api.LocationBackendService;
 import org.microg.nlp.api.LocationHelper;
 
-import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
+import android.app.Service;
+import android.content.Intent;
 import android.util.Log;
 
 public class GSMService extends LocationBackendService {
@@ -20,60 +18,53 @@ public class GSMService extends LocationBackendService {
     protected Lock lock = new ReentrantLock();
     protected Thread worker = null;
 
-    protected void onOpen() {
-        super.onOpen();
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
 
-        Log.d(TAG, "Starting location backend via looper");
+        Log.d(TAG, "Starting location backend");
+        CellbasedLocationProvider.getInstance().init(getApplicationContext());
+        try {
+            lock.lock();
+            if (worker != null) worker.interrupt();
+            worker = new Thread() {
+                public void run() {
+                    Log.d(TAG, "Starting reporter thread");
+                    CellbasedLocationProvider lp =
+                        CellbasedLocationProvider.getInstance();
+                    double lastLng = 0d;
+                    double lastLat = 0d;
+                    try { while (true) {
+                        Thread.sleep(1000);
 
-        Looper looper = getApplication().getMainLooper();
-        final Context applicationContext = getApplicationContext();
+                        CellInfo[] infos = lp.getAll();
 
-        Handler handler = new Handler(looper) {
-            public void handleMessage(Message msg) {
-                Log.d(TAG, "Starting location backend in \"UI\" thread");
-                CellbasedLocationProvider.getInstance().init(applicationContext);
-                try {
-                    lock.lock();
-                    if (worker != null) worker.interrupt();
-                    worker = new Thread() {
-                        public void run() {
-                            Log.d(TAG, "Starting reporter thread");
-                            CellbasedLocationProvider lp =
-                                CellbasedLocationProvider.getInstance();
-                            double lastLng = 0d;
-                            double lastLat = 0d;
-                            try { while (true) {
-                                Thread.sleep(1000);
+                        if (infos.length == 0) continue;
 
-                                CellInfo[] infos = lp.getAll();
-
-                                if (infos.length == 0) continue;
-
-                                double lng = 0d;
-                                double lat = 0d;
-                                for(CellInfo c : infos) {
-                                    lng += c.lng;
-                                    lat += c.lat;
-                                }
-                                lng /= infos.length;
-                                lat /= infos.length;
-                                float acc = (float)(800d / infos.length);
-                                if (lng != lastLng || lat != lastLat) {
-                                    Log.d(TAG, "report (" + lat + "," + lng + ")");
-                                    lastLng = lng;
-                                    lastLat = lat;
-                                    report(LocationHelper.create("gsm", lat, lng, acc));
-                                }
-                            } } catch (InterruptedException e) {}
+                        double lng = 0d;
+                        double lat = 0d;
+                        for(CellInfo c : infos) {
+                            lng += c.lng;
+                            lat += c.lat;
                         }
-                    };
-                    worker.start();
-                } finally {
-                    try { lock.unlock(); } catch (Exception e) {}
+                        lng /= infos.length;
+                        lat /= infos.length;
+                        float acc = (float)(800d / infos.length);
+                        if (lng != lastLng || lat != lastLat) {
+                            Log.d(TAG, "report (" + lat + "," + lng + ")");
+                            lastLng = lng;
+                            lastLat = lat;
+                            report(LocationHelper.create("gsm", lat, lng, acc));
+                        }
+                    } } catch (InterruptedException e) {}
                 }
-            }
-        };
-        handler.handleMessage(new Message());
+            };
+            worker.start();
+        } finally {
+            try { lock.unlock(); } catch (Exception e) {}
+        }
+
+        return Service.START_STICKY;
     }
 
     protected void onClose() {
